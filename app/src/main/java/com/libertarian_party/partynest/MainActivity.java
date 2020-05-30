ffab2058aaa442186be8de9ceb84d011826886ca
Alex Kotov <kotovalexarian@gmail.com>
2020-05-30 13:15:03 +0500

    Add two integer numbers

--- app/src/main/java/com/libertarian_party/partynest/MainActivity.java
+++ app/src/main/java/com/libertarian_party/partynest/MainActivity.java
@@ -3,6 +3,10 @@ package com.libertarian_party.partynest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addButton = (Button)findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumberEditText  = (EditText)findViewById(R.id.firstNumberEditText);
+                EditText secondNumberEditText = (EditText)findViewById(R.id.secondNumberEditText);
+                TextView resultTextView       = (TextView)findViewById(R.id.resultTextView);
+
+                int firstNumber  = Integer.parseInt(firstNumberEditText.getText().toString());
+                int secondNumber = Integer.parseInt(secondNumberEditText.getText().toString());
+                int result = firstNumber + secondNumber;
+
+                resultTextView.setText(String.valueOf(result));
+            }
+        });
     }
 }
