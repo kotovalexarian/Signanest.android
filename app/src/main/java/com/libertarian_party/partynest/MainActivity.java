f7b8d5399ce42ae4bf61f9ce1a55acde9be29070
Alex Kotov <kotovalexarian@gmail.com>
2020-05-31 01:34:41 +0500

    Add list

--- app/src/main/java/com/libertarian_party/partynest/MainActivity.java
+++ app/src/main/java/com/libertarian_party/partynest/MainActivity.java
@@ -1,13 +1,37 @@
 package com.libertarian_party.partynest;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView keyAliasesListView;
+
+    private void setWidgetInstanceVariables() {
+        keyAliasesListView = (ListView)findViewById(R.id.keyAliasesListView);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setWidgetInstanceVariables();
+
+        final String[] itemsArray = new String[] {
+                "Hello, World!",
+                "Some text",
+                "Foo Bar",
+                "Zoo Car",
+        };
+
+        final ArrayAdapter<String> itemsArrayAdapter = new ArrayAdapter<>(
+                this,
+                R.layout.simple_text_list,
+                R.id.textView,
+                itemsArray);
+
+        keyAliasesListView.setAdapter(itemsArrayAdapter);
     }
 }
