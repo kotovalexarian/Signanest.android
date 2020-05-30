83f7f54e5a141eb3bc7a8131085991eaf0049129
Alex Kotov <kotovalexarian@gmail.com>
2020-05-31 00:06:59 +0500

    Remove unnecessary code

--- app/src/main/java/com/libertarian_party/partynest/MainActivity.java
+++ app/src/main/java/com/libertarian_party/partynest/MainActivity.java
@@ -1,9 +1,6 @@
 package com.libertarian_party.partynest;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,27 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button addActivityButton  = (Button)findViewById(R.id.addActivityButton);
-        Button listActivityButton = (Button)findViewById(R.id.listActivityButton);
-
-        addActivityButton.setOnClickListener(onAddActivityButtonClick);
-        listActivityButton.setOnClickListener(onListActivityButtonClick);
     }
-
-    private View.OnClickListener onAddActivityButtonClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent startIntent = new Intent(getApplicationContext(), AddActivity.class);
-            startActivity(startIntent);
-        }
-    };
-
-    private View.OnClickListener onListActivityButtonClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent startIntent = new Intent(getApplicationContext(), ListActivity.class);
-            startActivity(startIntent);
-        }
-    };
 }
