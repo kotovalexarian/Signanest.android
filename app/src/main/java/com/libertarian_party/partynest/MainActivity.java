da1b435a9c3d139cea9b69324b4ab930036fd5dd
Alex Kotov <kotovalexarian@gmail.com>
2020-05-31 01:48:39 +0500

    Load key aliases

--- app/src/main/java/com/libertarian_party/partynest/MainActivity.java
+++ app/src/main/java/com/libertarian_party/partynest/MainActivity.java
@@ -6,6 +6,14 @@ import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+
 public class MainActivity extends AppCompatActivity {
     private ListView keyAliasesListView;
 
@@ -19,12 +27,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setWidgetInstanceVariables();
 
-        final String[] itemsArray = new String[] {
-                "Hello, World!",
-                "Some text",
-                "Foo Bar",
-                "Zoo Car",
-        };
+        ArrayList<String> aliasesArrayList = new ArrayList<>();
+
+        try {
+            KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
+            keyStore.load(null);
+
+            Enumeration<String> aliasesEnumeration = keyStore.aliases();
+            while (aliasesEnumeration.hasMoreElements())
+                aliasesArrayList.add(aliasesEnumeration.nextElement());
+
+        } catch (KeyStoreException | CertificateException | IOException | NoSuchAlgorithmException e) {
+            // Do nothing.
+        }
+
+        String[] itemsArray = new String[aliasesArrayList.size()];
+        itemsArray = aliasesArrayList.toArray(itemsArray);
 
         final ArrayAdapter<String> itemsArrayAdapter = new ArrayAdapter<>(
                 this,
