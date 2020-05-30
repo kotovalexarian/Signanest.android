82f9b119e394ee1ee2337b6bdcaaf3ac2c617c07
Alex Kotov <kotovalexarian@gmail.com>
2020-05-30 13:45:37 +0500

    Two activities

--- app/src/main/java/com/libertarian_party/partynest/MainActivity.java
+++ app/src/main/java/com/libertarian_party/partynest/MainActivity.java
@@ -1,33 +1,24 @@
 package com.libertarian_party.partynest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button addButton = (Button)findViewById(R.id.addButton);
-        addButton.setOnClickListener(new View.OnClickListener() {
+        Button addActivityButton = (Button)findViewById(R.id.addActivityButton);
+        addActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText firstNumberEditText  = (EditText)findViewById(R.id.firstNumberEditText);
-                EditText secondNumberEditText = (EditText)findViewById(R.id.secondNumberEditText);
-                TextView resultTextView       = (TextView)findViewById(R.id.resultTextView);
-
-                int firstNumber  = Integer.parseInt(firstNumberEditText.getText().toString());
-                int secondNumber = Integer.parseInt(secondNumberEditText.getText().toString());
-                int result = firstNumber + secondNumber;
-
-                resultTextView.setText(String.valueOf(result));
+                Intent startIntent = new Intent(getApplicationContext(), AddActivity.class);
+                startActivity(startIntent);
             }
         });
     }
